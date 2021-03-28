42fb6829a71d65b800bcae1224bb7ec0a460771c
ZainabBadawy <35436290+ZainabBadawy@users.noreply.github.com>
2021-03-28 17:08:04 -0400

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,12 @@
 import time
 import Adafruit_ADS1x15
+import RPi.GPIO as GPIO
+from pynput.keyboard import Key, Controller
+
+
+#for the keyboard
+GPIO.setmode(GPIO.BCM)
+keyboard = Controller()
 
 # Create an ADS1115 ADC (16-bit) instance.
 adcY = Adafruit_ADS1x15.ADS1115()   #y-axis
@@ -17,16 +24,24 @@ GAIN = 1
 print('Press Ctrl-C to quit...')
 while True:
 
-        start =time.time()
-        while(time.time() - start)<= 2.5:
-                if(adcY.read_adc(1,gain=GAIN) >=25000):
-                        print('joystick towards the user')
-                if(adcX.read_adc(2,gain=GAIN) <=10000):
-                        print('joystick right')
-                if(adcX.read_adc(2,gain=GAIN) >=25000):
-                        print('joystick left')
-                if(adcSW.read_adc(3,gain=GAIN) ==0):
-                        print('Button is pressed')
+    start =time.time()
+    while(time.time() - start)<= 5:
+        if(adcY.read_adc(1,gain=GAIN) >=25000):
+            print('joystick towards the user')
+            keyboard.press(Key.down)
+            keyboard.release(Key.down)
+        if(adcX.read_adc(2,gain=GAIN) <=10000):
+            print('joystick right')
+            keyboard.press(Key.ctrl_r)
+            keyboard.release(Key.ctrl_r)
+        if(adcX.read_adc(2,gain=GAIN) >=25000):
+            print('joystick left')
+            keyboard.press(Key.ctrl_l)
+            keyboard.release(Key.ctrl_l)
+        if(adcSW.read_adc(3,gain=GAIN) ==0):
+            print('Button is pressed')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
                 #value = adcY.get_last_result()
                 #print('Channel Y: {0}'.format(value))
-                time.sleep(0.5)
+            time.sleep(0.5)
