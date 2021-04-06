ab40e77ea0491126ca04c89c236f2b28bd7fe064
ZainabBadawy <35436290+ZainabBadawy@users.noreply.github.com>
2021-04-06 16:33:52 -0400

    Update db_insert.php

--- db_insert.php
+++ db_insert.php
@@ -4,14 +4,20 @@ include "db_connect.php";
 date_default_timezone_set('America/Toronto');  
 
 $phrase = $_GET["phrase"];
-
+$category=$_GET["category"];
 
 echo "<h2>Adding Phrase. Please wait. </h2>";
 echo "<h2>Phrase is:</h2>";
 echo "<h4>$phrase</h4>";
+echo "<h4>Phrase is added to: </h4>";
+echo "<h4>$category</h4>";
 
-$sql = "INSERT INTO simplePhrases (ID, PHRASES) VALUES (NULL,'$phrase')";
-
+if ($category == "TalkToPeople"){
+    $sql = "INSERT INTO simplePhrases (ID, PHRASES) VALUES (NULL,'$phrase')";
+}
+if($category == "TalkToDevices"){
+    $sql = "INSERT INTO homeDevices (ID, Phrase) VALUES (NULL,'$phrase')";
+}
 if ($result = $mysqli->query($sql) === TRUE){
     echo "New record created successfully";
 } else{
